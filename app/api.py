ad3ed9bcd4c619c70a2b638753fa6fd11afe8080
alex19bh <alejandro.ortiz.salduba@gmail.com>
2025-05-11 05:33:01 +0200

    Añadir multiplicacion y division

--- app/api.py
+++ app/api.py
@@ -1,7 +1,5 @@
 import http.client
-
 from flask import Flask
-
 from app import util
 from app.calc import Calculator
 
@@ -31,3 +29,29 @@ def substract(op_1, op_2):
         return ("{}".format(CALCULATOR.substract(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+# Ruta para multiplicación
+@api_application.route("/calc/multiply/<op_1>/<op_2>", methods=["GET"])
+def multiply(op_1, op_2):
+    try:
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        return ("{}".format(CALCULATOR.multiply(num_1, num_2)), http.client.OK, HEADERS)
+    except TypeError as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+# Ruta para división
+@api_application.route("/calc/divide/<op_1>/<op_2>", methods=["GET"])
+def divide(op_1, op_2):
+    try:
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        return ("{}".format(CALCULATOR.divide(num_1, num_2)), http.client.OK, HEADERS)
+    except ZeroDivisionError as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+    except TypeError as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+if __name__ == "__main__":
+    api_application.run(debug=True, host="0.0.0.0", port=5000)
